39e0462ce95694703805c1a7753e8220656d8c6f
Anne <mediolanuma@gmail.com>
2023-05-07 19:40:49 +0300

    Initial commit

---
+++ logistic/views.py
@@ -0,0 +1,24 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['tilte', 'description']
+    search_fields = ['tilte', 'description']
+    pagination_class = LimitOffsetPagination
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    search_fields = ['products']
+    pagination_class = LimitOffsetPagination
+    # при необходимости добавьте параметры фильтрации
